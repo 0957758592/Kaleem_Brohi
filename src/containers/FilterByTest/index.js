74d726df83b905dbd0d2a8442e423b3abb6cd318
Anton <arnellomutty2@gmail.com>
2019-12-16 15:35:38 +0200

    Select containers and filter date completed

--- src/containers/FilterByTest/index.js
+++ src/containers/FilterByTest/index.js
@@ -1,42 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from "react";
+import SelectItem from "@/components/Select";
 
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 200,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
+const test = [
+  { name: "All" },
+  { name: "Math" },
+  { name: "Reading" },
+];
 
 export default function FilterByTest() {
-  const classes = useStyles();
-  const [age, setAge] = React.useState('');
-  const handleChange = event => {
-    setAge(event.target.value);
-  };
-
-  return (
-    <div>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Data by Test</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>All</MenuItem>
-          <MenuItem value={20}>Math</MenuItem>
-          <MenuItem value={30}>Reading</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
-  );
+  return <SelectItem name="Test" options={test} />;
 }
--- src/containers/FilterDownload/index.js
+++ src/containers/FilterDownload/index.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import { compose, withProps } from 'recompose';
-import { inject, observer } from 'mobx-react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { compose, withProps } from "recompose";
+import { inject, observer } from "mobx-react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { getCSVFile } from "@/stores/wrd_apis";
 
-import { STORE_KEYS } from '@/stores';
+import { STORE_KEYS } from "@/stores";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
       width: 250,
-      height: 50,
-    },
-  },
+      height: 50
+    }
+  }
 }));
 
-const FilterDownload = ({ getDownloadFile }) => {
+const FilterDownload = ({ getDownloadFile, postQuery }) => {
   const classes = useStyles();
+
+  const handleDownload = async () => {
+    let query = "";
+    for (let [key, value] of Object.entries(postQuery)) {
+      query = query
+        ? query.concat("", `&${key}=${value}`)
+        : query.concat("", `${key}=${value}`);
+    }
+    getCSVFile().then(data => {
+      getDownloadFile(data);
+    });
+  };
 
   return (
     <div className={classes.root}>
-      <Button variant="contained" onClick={() => {
-        getDownloadFile();
-      }}>Download</Button>
+      <Button variant="contained" onClick={handleDownload}>
+        Download
+      </Button>
     </div>
   );
-}
+};
 
 export default compose(
   inject(STORE_KEYS.VIEWMODESTORE),
   observer,
   withProps(
-    ({
-       [STORE_KEYS.VIEWMODESTORE]: { getDownloadFile },
-     }) => ({
+    ({ [STORE_KEYS.VIEWMODESTORE]: { getDownloadFile, postQuery } }) => ({
       getDownloadFile,
+      postQuery
     })
   )
 )(FilterDownload);
