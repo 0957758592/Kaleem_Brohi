16cca482246e03c3d6e3f9f1098bb6d40809dab0
brohik <brohik@rv-C02ZD4TZLVDR.local>
2019-12-16 15:36:17 +0200

    Changed api file

--- src/stores/wrd_apis/index.js
+++ src/stores/wrd_apis/index.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getHeaders = () => ({
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   }
 });
 
@@ -10,14 +10,14 @@ export const getFilterOptionsDistrict = () =>
   axios
     // .get(`http://127.0.0.1:9202/search?level=${mode}`)
 
-// http://localhost:9202/adaptive/v1/research/filters?level=district
-// http://localhost:9202/adaptive/v1/research/filters?level=building&ids=635568,635504,619716,635650,635603,635586,619594
-// http://localhost:9202/adaptive/v1/research/filters?level=class&ids=635652,635570,619717
+    // http://localhost:9202/adaptive/v1/research/filters?level=district
+    // http://localhost:9202/adaptive/v1/research/filters?level=building&ids=635568,635504,619716,635650,635603,635586,619594
+    // http://localhost:9202/adaptive/v1/research/filters?level=class&ids=635652,635570,619717
 
     .get(`http://localhost:9202/adaptive/v1/research/filters?level=district`)
     .then(res => res.data || []);
 
-export const getFilterOptionsBuilding = (ids) =>
+export const getFilterOptionsBuilding = ids =>
   axios
     // .get(`http://127.0.0.1:9202/search?level=${mode}`)
 
@@ -25,35 +25,37 @@ export const getFilterOptionsBuilding = (ids) =>
     // http://localhost:9202/adaptive/v1/research/filters?level=building&ids=635568,635504,619716,635650,635603,635586,619594
     // http://localhost:9202/adaptive/v1/research/filters?level=class&ids=635652,635570,619717
 
-    .get(`http://localhost:9202/adaptive/v1/research/filters?level=building&ids=${ids}`)
-    .then(res => res.data || []);
-
-export const getFilterOptionsClass = (ids) =>
-  axios
-    // .get(`http://127.0.0.1:9202/search?level=${mode}`)
-
-    // http://localhost:9202/adaptive/v1/research/filters?level=district
-    // http://localhost:9202/adaptive/v1/research/filters?level=building&ids=635568,635504,619716,635650,635603,635586,619594
-    // http://localhost:9202/adaptive/v1/research/filters?level=class&ids=635652,635570,619717
-
-    .get(`http://localhost:9202/adaptive/v1/research/filters?level=class&ids=${ids}`)
-    .then(res => res.data || []);
-
-export const getCSVFile = (data) =>
-  axios
-    .post(
-      `http://127.0.0.1:9201/filter/`,
-      {
-        bopdy: mode,
-        
-        // Todo: replace data with parameter
-        // "building": [
-        //   "QABS", "RIVB"
-        // ],
-        // "className": [
-        //   "RIVC-3", "RIVC-5"
-        // ]
-      },
-      getHeaders()
+    .get(
+      `http://localhost:9202/adaptive/v1/research/filters?level=building&ids=${ids}`
     )
-    .then(res => res.data);
+    .then(res => res.data || []);
+
+export const getFilterOptionsClass = ids =>
+  axios
+    // .get(`http://127.0.0.1:9202/search?level=${mode}`)
+
+    // http://localhost:9202/adaptive/v1/research/filters?level=district
+    // http://localhost:9202/adaptive/v1/research/filters?level=building&ids=635568,635504,619716,635650,635603,635586,619594
+    // http://localhost:9202/adaptive/v1/research/filters?level=class&ids=635652,635570,619717
+
+    .get(
+      `http://localhost:9202/adaptive/v1/research/filters?level=class&ids=${ids}`
+    )
+    .then(res => res.data || []);
+
+export const getCSVFile = data =>
+  axios
+    .get(`http://localhost:9201/adaptive/v1/research?${data}`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Credentials": true
+      },
+      fetchOptions: {
+        mode: "cors"
+      }
+    })
+    .then(res => {
+      console.log("resp", res)
+      // res.data
+    } );
