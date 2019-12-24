4e41ea1123d00c5c6311d213e874f62fd211ee04
Дмитрий Кока <dmitrykoka@gmail.com>
2019-12-24 22:10:41 +0200

    Changed styles layout

--- src/layouts/basic/index.js
+++ src/layouts/basic/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
 
 import FilterByDate from "@/containers/FilterByDate";
 import FilterByTest from "@/containers/FilterByTest";
@@ -13,11 +14,41 @@ import FilterDownload from "@/containers/FilterDownload";
 
 import { Wrapper } from "./styles";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    padding: theme.spacing(1),
+    margin: "0 auto",
+    fontSize: "1em",
+    [theme.breakpoints.between('xs', 'sm')]: {
+      minWidth: "70%",
+      maxWidth: "80%",
+    },
+    [theme.breakpoints.between('md', 'lg')]: {
+      minWidth: "50%",
+      maxWidth: "60%",
+    },
+    [theme.breakpoints.up('xl')]: {
+      minWidth: "30%",
+      maxWidth: "40%",
+    },
+  },
+  button: {
+    width: 260,
+    margin: "0 auto"
+  }
+}));
 export default () => {
+  const classes = useStyles();
+
   return (
     <Wrapper>
-      <Grid container alignItems="center" justify="center">
-        <Grid item xl={3} lg={3} md={6} sm={9} xs={10}>
+      <div className={classes.root}>
+      {/*<Grid container alignItems="center" justify="center">*/}
+      {/*  <Grid item xl={3} lg={3} md={6} sm={9} xs={10}>*/}
           <h1>Research Data</h1>
           <FilterByDate />
           <FilterByTest />
@@ -27,9 +58,12 @@ export default () => {
           <FilterByBuilding />
           <FilterByClass />
           <FilterByStudent />
+          <div className={classes.button}>
           <FilterDownload />
-        </Grid>{" "}
-      </Grid>
+          </div>
+      {/*  </Grid>{" "}*/}
+      {/*</Grid>*/}
+      </div>
     </Wrapper>
   );
 };
